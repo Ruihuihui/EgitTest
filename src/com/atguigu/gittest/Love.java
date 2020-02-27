b24833dce2f615d4f3cdba8a3c57444dd2717f6f
芮辉辉 <rui19960306@163.com>
2020-02-27 13:17:06 +0800

    1111111111 2222222222 333333333 444444444

--- src/com/atguigu/gittest/Love.java
+++ src/com/atguigu/gittest/Love.java
@@ -4,6 +4,10 @@ public class Love {
 	public static void main(String[] args) {
 		System.out.println("i love you ");
 		System.out.println("i love meet you ");
+		System.out.println("11111111111111 ");
+		System.out.println("222222222222 ");
+		System.out.println("3333333333333333333 ");
+		System.out.println("44444444444444444444444444 ");
 		
 	}
 }
