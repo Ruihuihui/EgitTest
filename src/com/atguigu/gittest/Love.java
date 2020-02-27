a6bfb609a1e09a9531e65961ca4c47884966b75f
芮辉辉 <rui19960306@163.com>
2020-02-27 13:01:01 +0800

    我是尚硅谷最帅

--- src/com/atguigu/gittest/Love.java
+++ src/com/atguigu/gittest/Love.java
@@ -3,7 +3,8 @@ package com.atguigu.gittest;
 public class Love {
 	public static void main(String[] args) {
 		System.out.println("i love you ");
-		System.out.println("i love meet you ");
+		System.out.println("i love meet you ");		
+		System.out.println("where meet you tonight ");
 		
 	}
 }
